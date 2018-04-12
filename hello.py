5d59897d9ade1442556f5cc021a544c4a408104b
Philip Scott <pscott@zeptohost.com>
2018-04-11 18:49:44 -0700

    feat(static): Add static file hosting

--- hello.py
+++ hello.py
@@ -1,6 +1,17 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return app.send_static_file('index.html')
+
+@app.route('/api')
+def hello_api():
+    return 'Hello, API!'
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+if __name__ == "__main__":
+    app.run()
