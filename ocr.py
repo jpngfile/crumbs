96795cff7ed5aaf9debe85436ec8977fd18f7695
Jason P'ng <jsjpng@gmail.com>
2018-04-11 19:19:21 -0700

    Cleaned up OCR test script

--- ocr.py
+++ ocr.py
@@ -3,20 +3,20 @@ import requests
 
 subscription_key = "e4f9a2574d75482597cd04bb93cfb3e0"
 vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/"
-vision_analyze_url = vision_base_url + "analyze"
 ocr_url = vision_base_url + "ocr"
 
-#image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Broadway_and_Times_Square_by_night.jpg/450px-Broadway_and_Times_Square_by_night.jpg"
-#image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png"
-
-
 headers  = {'Ocp-Apim-Subscription-Key': subscription_key }
-#params   = {'visualFeatures': 'Categories,Description,Color'}
 params   = {'language': 'unk', 'detectOrientation ': 'true'}
-#data     = {'url': image_url}
-files = {'file': open('legendMoviePoster.jpg', 'rb')}
+files = {'file': open('testImages/stopSign.png', 'rb')}
 response = requests.post(ocr_url, headers=headers, params=params, files=files)
 response.raise_for_status()
 analysis = response.json()
 
-print(analysis)
+line_infos = [region["lines"] for region in analysis["regions"]]
+word_infos = []
+for line in line_infos:
+    for word_metadata in line:
+        for word_info in word_metadata["words"]:
+            word_infos.append(word_info)
+
+print(word_infos)
