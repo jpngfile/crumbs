480d12a6fe7ce8877b6a1c2df3d584b41846f476
Philip Scott <pscott@zeptohost.com>
2018-04-12 15:11:04 -0700

    feat(upload): Add upload /POST endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
@@ -7,7 +7,25 @@ def home():
 
 @app.route('/api')
 def hello_api():
-    return 'Hello, API!'
+    return jsonify(
+            message='Hello, API!')
+
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    if 'team' not in request.form:
+        return jsonify(
+            error='No team specified.')
+    if 'file' not in request.files:
+        return jsonify(
+            error='No file sent.')
+
+    scaled_file = scale_image(request.files[file])
+    image_json = ml_scan_image(scaled_file)
+    payload = parse_image_json(image_json)
+
+    cache_payload(payload, request.form['team'])
+
+    return payload
 
 @app.route('/static/<path:path>')
 def send_static(path):
@@ -15,3 +33,8 @@ def send_static(path):
 
 if __name__ == "__main__":
     app.run()
+
+def scale_image(file): pass
+def ml_scan_image(file): pass
+def parse_image_json(json): pass
+def cache_payload(json, team): pass
