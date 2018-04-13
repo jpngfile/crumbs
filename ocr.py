610a57860af7c81155d1c64e956bad679d516a79
Jason P'ng <jsjpng@gmail.com>
2018-04-13 10:37:15 -0700

    feat(upload): Finished endpoint for uploading images (#22)

--- ocr.py
+++ ocr.py
@@ -64,6 +64,31 @@ def getScrumDict(word_list):
         scrum_dict[matching_header].append(word[u'text'])
     return scrum_dict
 
+def getAzureAnalysis(image_data):
+    subscription_key = "e4f9a2574d75482597cd04bb93cfb3e0"
+    vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/"
+    text_recognition_url = vision_base_url + "recognizeText"
+    headers    = {
+            'Ocp-Apim-Subscription-Key': subscription_key,
+            "Content-Type": "application/octet-stream"
+            }
+    params = {'handwriting': True}
+    response = requests.post(text_recognition_url, headers=headers, params=params, data=image_data)
+    response.raise_for_status()
+    operation_url = response.headers["Operation-Location"]
+    print(operation_url)
+
+    analysis = {}
+    while not "recognitionResult" in analysis:
+        response_final = requests.get(operation_url, headers=headers)
+        analysis = response_final.json()
+        time.sleep(1)
+    return analysis
+
+def parseAnalysis(analysis):
+    word_list = getWords(analysis)
+    return getScrumDict(word_list)
+
 def getSampleAnalysis():
     subscription_key = "e4f9a2574d75482597cd04bb93cfb3e0"
     vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/"
