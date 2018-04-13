c972ab1e25280eb7661658cdbbac98b97e451736
Jason P'ng <jsjpng@gmail.com>
2018-04-12 22:53:27 -0700

    Removed unused files

--- client/init.js
+++
@@ -1,7 +0,0 @@
-$('#file-upload').bind('change', function() {
-    var fileName="gbiib";
-    console.log("File change");
-    fileName = $(this).val();
-    $('#file-selected').html(fileName);
-})
-console.log("Included bind");
