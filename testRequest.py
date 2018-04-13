b75e288f0925fb00ed63108b1e388c4f80c331c6
Jason P'ng <jsjpng@gmail.com>
2018-04-13 11:41:00 -0700

    Added test scrumboard

--- testRequest.py
+++ testRequest.py
@@ -5,7 +5,8 @@ import requests
 crumbs_url = "http://127.0.0.1:5000/"
 upload_url = crumbs_url + "api/upload"
 
-image_path = "testImages/dummyScrumboard.jpg"
+#image_path = "testImages/simpleScrumboard2.jpg"
+image_path = "testImages/simpleScrumboard.jpg"
 image_file = open(image_path, 'rb')
 files = {'file': image_file}
 data={'team': 'cookies'}
