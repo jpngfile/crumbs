faaebb49a7faee06a3f9f41ebb6488a21329f291
Philip Scott <pscott@zeptohost.com>
2018-04-13 07:15:37 -0700

    feat(render): Add UI logic for rendering payload (#19)

--- client/app.js
+++ client/app.js
@@ -1,7 +1,59 @@
 import { h, app } from 'hyperapp'
+import { location, Route } from '@hyperapp/router'
 
-const state = {}
-const actions = {}
+const state = {
+    board: {
+        team: 'not loaded',
+        headers: {
+            test: ['string', 'aaa']
+        }
+    },
+    location: location.state
+}
+
+const actions = {
+    getTeam: async (team) => {
+        const payload = await fetch(`http://localhost:5000/api/${team}`)
+        const board = await payload.json()
+        actions.setBoard(board)
+    },
+    setBoard: board => ({ board: 5 }),
+    location: location.actions
+}
+
+const Sticky = ({ content }) => (
+    <li>{content}</li>
+)
+
+const Header = ({ header, stickies }) => (
+    <div>
+        <h3>{header}</h3>
+        <ul>
+            {stickies.map((sticky) => (
+            <Sticky content={sticky} />
+            ))}
+        </ul>
+    </div>
+)
+
+const Board = ({ team, headers }) => (
+    <div>
+        <h1>{team}</h1>
+        <div>
+            {Object.keys(headers).map((key) => (
+            <Header header={key} stickies={headers[key]} />
+            ))}
+        </div>
+    </div>
+)
+
+const App = (state, actions) => ({ location, match }) => {
+    actions.getTeam(match.params.team)
+
+    return (
+        <Board team={state.board.team} headers={state.board.headers} />
+    )
+}
 
 const view = (state, actions) => (
     <div>
@@ -11,7 +63,10 @@ const view = (state, actions) => (
             <input type="text" name="team" placeholder="Team name!" />
             <input type="submit" value="Scrum!" />
         </form>
+
+        <Route path="/board/:team" state={state} actions={actions} render={App(state, actions)} />
     </div>
 )
 
-app(state, actions, view, document.body)
+const main = app(state, actions, view, document.body)
+const unsubscribe = location.subscribe(main.location)
